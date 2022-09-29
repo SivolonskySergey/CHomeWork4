790fc0716329d6a367e9381152b27856873227b9
Sergey <bibert.shmibert@gmail.com>
2022-09-29 13:32:27 +0400

    add exercise 25

---
+++ Exercise25/Program.cs
@@ -0,0 +1,13 @@
+﻿
+void Exponentiate(int A, int B) 
+{
+    int currentA = A;
+    for(int i = 1; i < B; i++) 
+    {
+        currentA = currentA * A;
+    }
+    Console.WriteLine(currentA);
+}
+
+Exponentiate(3, 5);
+Exponentiate(2, 4);
