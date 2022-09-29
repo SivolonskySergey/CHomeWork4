82db50c9c2262e9bfd87a635ec9c66607cd13119
Sergey <bibert.shmibert@gmail.com>
2022-09-29 20:40:48 +0400

    add Exercises 27, 28

---
+++ Exercise27/Program.cs
@@ -0,0 +1,36 @@
+﻿
+void showArray(int[] arr) 
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int getLength(int num) {
+    int count = 0;
+    while( num > 0) {
+        num = num / 10;
+        count+= 1;
+    }
+    return count;
+}
+
+void sumOfDigits(int number) 
+{
+    int lastDigit = number % 10;              
+    
+    int length = getLength(number);
+    double result = 0;
+    for(int i = 0; i <= length ; i++)
+    {        
+        result = result + (number % 10);
+        number = number / 10;
+    }
+    Console.WriteLine(result);
+}
+sumOfDigits(452);
+sumOfDigits(82);
+sumOfDigits(9012);
+
---
+++ Exercise28/Program.cs
@@ -0,0 +1,42 @@
+﻿
+void showArray(int[] arr) 
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+};
+
+void getSorted(int[] array) 
+{
+    int temp;
+    for(int i = 1; i < array.Length; i++) 
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(Math.Abs(array[i]) < Math.Abs(array[j]))
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }   
+    }
+    showArray(array);
+};
+
+void sortSomeArray() 
+{
+    Random rnd = new Random();
+    int length = 8;
+    int[] randomDigits = new int[length];
+    for(int i = 0; i < length; i++) 
+    {
+        randomDigits[i] = rnd.Next(-100, 100);
+    }
+    showArray(randomDigits);
+    getSorted(randomDigits);
+}
+
+sortSomeArray();
